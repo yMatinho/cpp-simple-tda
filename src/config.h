78214ceb0a0cd2a0cb2b6e2036c0e1d71c19996e
Matheus Acioli <matheusmaceio2013@gmail.com>
2023-08-30 12:59:08 -0300

    feat: Database Connector

---
+++ src/config.h
@@ -0,0 +1,12 @@
+#include <string>
+
+using namespace std;
+
+namespace Constants
+{
+    extern const string MYSQL_HOST = "127.0.0.1";
+    extern const string MYSQL_DATABASE = "tda";
+    extern const int MYSQL_PORT = 3306;
+    extern const string MYSQL_USER = "user";
+    extern const string MYSQL_PASSWORD = "password";
+}
